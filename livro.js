4c4619da4105f3350d58e06f6b215bf55233cec2
Caio Fonseca <fcaio8137@gmail.com>
2019-08-07 22:29:57 -0300

    Criação de validação de formulario com jquery validation

--- livro.js
+++ livro.js
@@ -1,19 +1,54 @@
 window.onload = () => {
   let elementoBotao = document.querySelector('#cadastrar')
 
-  elementoBotao.addEventListener('click', (event) => {
-    event.preventDefault()
-   console.log('Clicou no botão')
-   let ladda = Ladda.create(elementoBotao)
-   ladda.start()
-   let progressoAtual = 0
-   let interval =
-   setInterval(() => {
-     ladda.setProgress(progressoAtual += 0.1)
-     if (progressoAtual >= 1) {
-       clearInterval(interval)
-       ladda.stop()
-     }
-   }, 200)
-  })
+  // elementoBotao.addEventListener('click', (event) => {
+  //   event.preventDefault()
+  //  console.log('Clicou no botão')
+  //  let ladda = Ladda.create(elementoBotao)
+  //  ladda.start()
+  //  let progressoAtual = 0
+  //  let interval =
+  //  setInterval(() => {
+  //    ladda.setProgress(progressoAtual += 0.1)
+  //    if (progressoAtual >= 1) {
+  //      clearInterval(interval)
+  //      ladda.stop()
+  //    }
+  //  },200)
+  // })
+
+  let formularioLivro = $('#formLivro')
+  formularioLivro.validate({
+    rules: {
+      titulo: {
+        required: true,
+        minlength: 3
+      },
+      subtitulo: {
+        required: false,
+        minlength: 5
+      },
+      descricao: {
+        required: true,
+        minlength:10
+      }
+    },
+    messages: {
+      titulo: {
+        required: 'O titulo é obrigatório.',
+        minlength: 'O titulo é muito curto.'
+      },
+      subtitulo: {
+        minlength: 'O subtitulo é muito curto.'
+      },
+      descricao: {
+        required: 'A descrição é obrigatória.',
+        minlength: 'A descrição deve ter mais de 10 caracteres.'
+      }
+    },
+    errorPlacement: function(error, element) {
+      element.parent().parent().find('.error').append(error).addClass('animated flash')
+    },
+    errorClass: 'cor-do-erro'
+   })
 }
