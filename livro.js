703aad2a95b0395e03f6ecc376b18652dd530fea
Caio Fonseca <fcaio8137@gmail.com>
2019-08-28 22:28:01 -0300

    conexao com a API

--- livro.js
+++ livro.js
@@ -55,11 +55,13 @@ window.onload = () => {
       let formSerializado = $(form).serialize()
       console.log(formSerializado)
 
-      fetch("http://localhost:8080/bookteca-api/src/teste.php").then(function(retorno){
-        return retorno.json()
-      }).then(function(json){
-        alert(json)
-      })
+     fetch('http://localhost:8080/bookteca-api/src/inserirlivro.php', {
+       method: "POST",
+       body: formSerializado,
+       headers: {
+         'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8'
+       }
+     }).then(console.log).catch(console.log)
     }
   })
 }
